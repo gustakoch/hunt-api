a2d2cd7c31380f7e4d7cb0d33d05e05c5ef6611c
Gustavo Koch <gustavok@outlook.com.br>
2019-06-20 22:57:12 -0300

    configura porta do heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const requireDir = require('require-dir');
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 const app = express();
 app.use(express.json()); //permite que a nossa aplicação envie arquivos no formato JSON
